8009fff10746ecd473b5b725b1e156b0195d3f47
samarpanbhujel <samarpanbhujel56@gmail.com>
2026-08-22 22:19:30 +0545

    q1

---
+++ Assignment-1/Q1_Set_Operations.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool contains(vector<int> set, int value) {
+    for (int x : set) {
+        if (x == value)
+            return true;
+    }
+    return false;
+}
+
+int main() {
+    vector<int> A = {1, 2, 3, 4};
+    vector<int> B = {3, 4, 5, 6};
+
+    // Union
+    cout << "Union: ";
+    for (int x : A)
+        cout << x << " ";
+
+    for (int x : B) {
+        if (!contains(A, x))
+            cout << x << " ";
+    }
+
+    // Intersection
+    cout << "\nIntersection: ";
+    for (int x : A) {
+        if (contains(B, x))
+            cout << x << " ";
+    }
+
+    // Difference A - B
+    cout << "\nDifference (A - B): ";
+    for (int x : A) {
+        if (!contains(B, x))
+            cout << x << " ";
+    }
+
+    // Difference B - A
+    cout << "\nDifference (B - A): ";
+    for (int x : B) {
+        if (!contains(A, x))
+            cout << x << " ";
+    }
+
+    // Cartesian Product
+    cout << "\nCartesian Product (A x B):\n";
+    for (int x : A) {
+        for (int y : B) {
+            cout << "(" << x << ", " << y << ") ";
+        }
+    }
+
+    return 0;
+}
